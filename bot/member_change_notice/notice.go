994d82e5bb99ca9d8fe2cb319c0ce5680d64ceed
wangli <w45241397@163.com>
2022-10-10 20:32:54 +0800

    fix: 提示@乱码

--- bot/member_change_notice/notice.go
+++ bot/member_change_notice/notice.go
@@ -8,7 +8,7 @@ import (
 func MemberJoinNotice(message *tools.MessageType) {
 	//群成员增加
 	if message.NoticeType == "group_increase" {
-		msg := fmt.Sprintf("欢迎[CQ:at,qq=%s]进群，请遵守群规范，关注群公告~\n来自bot", message.UserId)
+		msg := fmt.Sprintf("欢迎[CQ:at,qq=%d]进群，请遵守群规范，关注群公告~\n来自bot", message.UserId)
 		sendMsg := tools.GetGroupMsgStruct(msg, message.GroupId)
 		tools.MsgChan <- sendMsg
 	}
